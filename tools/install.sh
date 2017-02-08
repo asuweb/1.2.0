b56b896f3543d55efb43b70691582f0755a40c7e
Alan Urquhart <alan@asuweb.co.uk>
2017-02-08 07:53:57 +0000

    initial install script

--- tools/install.sh
+++ tools/install.sh
@@ -1,60 +1,165 @@
 #!/usr/bin/env bash
+REQPHPMODULES=()
+REQPHPMODULES+=('mysql')
+REQPHPMODULES+=('gd')
+REQPHPMODULES+=('mbstring')
+REQPHPMODULES+=('curl')
+REQPHPMODULES+=('zlib')
+REQPHPMODULES+=('ldap')
+
+version(){
+    local h t v
+
+    [[ $2 = "$1" || $2 = "$3" ]] && return 0
+
+    v=$(printf '%s\n' "$@" | sort -V)
+    h=$(head -n1 <<<"$v")
+    t=$(tail -n1 <<<"$v")
+
+    [[ $2 != "$h" && $2 != "$t" ]]
+}
 
 #Find OS
  if cat /etc/*release | grep ^NAME | grep CentOS; then
-    echo "==============================================="
-    echo "CentOS is supported"
-    echo "==============================================="
-    PACKAGE_MANAGER=yum
+    OS="CentOS"
+    PM="yum"
  elif cat /etc/*release | grep ^NAME | grep Red; then
-    echo "==============================================="
-    echo "Installing packages $YUM_PACKAGE_NAME on RedHat"
-    echo "==============================================="
-    PACKAGE_MANAGER=yum
+    OS="RedHat"
+    PM="yum"
  elif cat /etc/*release | grep ^NAME | grep Fedora; then
-    echo "================================================"
-    echo "Installing packages $YUM_PACKAGE_NAME on Fedorea"
-    echo "================================================"
-    PACKAGE_MANAGER=yum
+    OS="Fedora"
+    PM="yum"
  elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
-    echo "==============================================="
-    echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
-    echo "==============================================="
-    apt-get update
-    PACKAGE_MANAGER=apt
+    OS="Ubuntu"
+    PM="apt"
  elif cat /etc/*release | grep ^NAME | grep Debian ; then
-    echo "==============================================="
-    echo "Installing packages $DEB_PACKAGE_NAME on Debian"
-    echo "==============================================="
-    apt-get update
-    PACKAGE_MANAGER=apt
- elif cat /etc/*release | grep ^NAME | grep Mint ; then
-    echo "============================================="
-    echo "Installing packages $DEB_PACKAGE_NAME on Mint"
-    echo "============================================="
-    apt-get update
-    PACKAGE_MANAGER=apt
- elif cat /etc/*release | grep ^NAME | grep Knoppix ; then
-    echo "================================================="
-    echo "Installing packages $DEB_PACKAGE_NAME on Kanoppix"
-    echo "================================================="
-    apt-get update
-    PACKAGE_MANAGER=apt
+    OS="Debian"
+    PM="apt"
  else
-    echo "OS NOT DETECTED, couldn't install package $PACKAGE"
+    echo "OS NOT SUPPORTED - Please perform a manual install"
     exit 1;
  fi
+echo "This is the install script for MailWatch";echo;
+echo "The script will attempt to install all of the required packages and configure them for you";echo;
 
-#check php exists
-command -v php >/dev/null 2>&1 ||
-    {
-        echo "PHP not found - installing"
-        if [[ $PACKAGE_MANAGER eq "yum" ]]
-        then
-            yum install -y php php-mysql php-mbcrypt php-gettext php-gd
-        else
-            apt-get install php5 php5-mysql php5-mbcrypt php5-gettext php5-gd
-        fi
-    }
-command -v php >/dev/null 2>&1 || {echo "Unable to install PHP. Aborting" >&2; exit 1;}
+##check if mailscanner is installed
+#if [ ! -f "/etc/Mailscanner/Mailscanner.conf" ]; then
+#    echo "MailScanner config is missing";echo;
+#    echo "MailScanner must be installed before installing MailWatch";echo;
+#    echo "Aborting...";exit 1;
+#fi
+
+#check for webserver
+WEBSERVER=
+if [[ type "httpd" > /dev/null 2>&1 || type "apache2" > /dev/null 2>&1 ]]; then
+    WEBSERVER="apache"
+elif type "nginx" > /dev/null 2>&1; then
+    WEBSERVER="nginx"
+else
+    echo "We're unable to find your webserver.  We support Apache and Nginx";echo;
+    echo "Do you wish me to install a webserver?"
+    echo "1 - Apache"
+    echo "2 - Nginx"
+    echo "N - do not install or configure"
+    echo;
+    read -r -p "Select Webserver: " response
+    if [[ $response =~ ^([nN][oO])$ ]]; then
+       #do not install or configure webserver
+       WEBSERVER=
+    elif [ $response == 1 ]; then
+       #Apache
+       WEBSERVER="apache"
+       INSTALLWEBSERVER=1
+    elif [ $response == 2 ]; then
+       #Nginx
+       WEBSERVER="nginx"
+       INSTALLWEBSERVER=1
+    else
+       WEBSERVER=
+    fi
+fi
+
+#check php is installed
+if type "php" > /dev/null 2>&1; then
+    #php is installed, lets check the version
+    PHPMAX="5.9"
+    PHPMIN="5.3"
+    PHPVERSION=$(php -v|grep --only-matching --max-count=1 --perl-regexp "[5|7]\.\\d+\.\\d+")
+    if ! version $PHPMIN $PHPVERSION $PHPMAX; then
+        echo "Your PHP version ($PHPVERSION) is unsupported.  Supported PHP version between $PHPMIN and $PHPMAX";echo;
+        echo "Please upgrade your PHP version, and then re-run this script";exit 1;
+    else
+        #php version installed and version ok
+        PHP=1
+    fi
+else
+    echo "PHP not installed";echo;
+    echo "Would you like me to install PHP? ";echo;
+    read -r -p "Would you like me to install PHP? <Y/n> " response
+    if [[ $response =~ ^([nN][oO])$ ]]; then
+        echo "PHP is required to continue.  Please manually install PHP and then re-run this install script";echo;
+        echo "Aborting...";exit 1;
+    else
+        PHP=0
+        INSTALLPHP=1
+    fi
+fi
+
+#check mysql
+if ! type "mysqld" > /dev/null 2>&1; then
+    #mysqld missing
+    echo "Mysql Server not found";echo;
+    read -r -p "Would you like me to install mysql? <Y/n> " response
+    if [[ $response =~ ^([nN][oO])$ ]]; then
+        echo "MySQL is required to continue.  Please manually install MySQL and then re-run this install script";echo;
+        echo "Aborting...";exit 1;
+    else
+        MYSQL=0
+        INSTALLMYSQL=1
+    fi
+else
+    #possible version checks needed here! TODO:version checks
+    MYSQL=1
+fi
+
+
+#echo "Do you wish me to configure your webserver?";echo;
+#echo "1 - Apache"
+#echo "2 - Nginx"
+#echo "N - do not install or configure"
+#echo;
+#read -r -p "Select Webserver: " response
+#if [[ $response =~ ^([nN][oO])$ ]]; then
+#    #do not install or configure webserver
+#    WEBSERVER=
+#elif [ $response == 1 ]; then
+#    #Apache
+#    WEBSERVER="apache"
+#elif [ $response == 2 ]; then
+#    #Nginx
+#    WEBSERVER="nginx"
+#else
+#    WEBSERVER=
+#fi
+#
+#echo "Checking for required packages";echo;
+##check if mailscanner is installed
+#if [ ! -f "/etc/Mailscanner/Mailscanner.conf" ]; then
+#    echo "MailScanner config is missing";echo;
+#    echo "MailScanner must be installed before installing MailWatch";echo;
+#    echo "Aborting...";exit 1;
+#fi
+#
+##required packages
+#if [ $WEBSERVER == "apache" ]; then
+#    YUMPACKAGES="httpd mysql-server mysql-client php mod_php php-gd php-mysql php-mbstring";
+#    APTPACKAGES="apache2 libapache2-mod-php mysql php5 php5-gd php5-mysql"
+#elif [ $WEBSERVER == "nginx" ]; then
+#    YUMPACKAGES="nginx mysql-server mysql-client php php-fpm php-gd php-mysql php-mbstring";
+#    APTPACKAGES="nginx mysql php5 php5-fpm php5-gd php5-mysql"
+#fi
+
+
+
+
 
